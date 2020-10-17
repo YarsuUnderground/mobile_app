4818ca5657c0c3f94c3a26050199a7b72ee7cd46
Vinogradov Alexander <astronom_35@mail.ru>
2020-10-17 20:24:50 +0300

    wtf build

---
+++ lib/nfc_connect.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:nfc/nfc.dart';
+
+class NfcConnection extends StatefulWidget {
+  @override
+  _NfcConnectionState createState() => _NfcConnectionState();
+}
+
+class _NfcConnectionState extends State<NfcConnection> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
